c8161364af0a2565662936df0ae4c4d1b12f9d2e
Bosco Ho <apposite@gmail.com>
2023-01-18 13:53:36 +1100

    fix: change lounge vm location

--- upload_lounge.sh
+++ upload_lounge.sh
@@ -1,2 +1,2 @@
 ./build_lounge_client.sh
-rsync -avz --progress --exclude '.DS_Store' --exclude 'node_modules' * bosco@216.153.60.196:rshow
+rsync -avz --progress --exclude '.DS_Store' --exclude 'node_modules' * bosco@216.153.60.196:rs/rshow
