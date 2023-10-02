cc19287914e5d023cede47a53dfbda32a47370c7
Bosco Ho <apposite@gmail.com>
2023-10-03 09:46:15 +1100

    debug: config

--- client/src/modules/rpc.js
+++ client/src/modules/rpc.js
@@ -4,7 +4,7 @@ import config from "../../../config/config.json";
 const defaultRemoteUrl = `${location.protocol}//${location.host}/rpc-run`;
 const remoteUrl = config.apiUrl;
 console.log(`rpc.defaultRemoteUrl=${defaultRemoteUrl}`);
-console.log(`config=${config}`);
+console.log(`config=${JSON.stringify(config)}`);
 console.log(`rpc.remoteUrl=${remoteUrl}`);
 
 /**
