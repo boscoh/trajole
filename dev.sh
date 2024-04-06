763f824a1f31ff5ea6ea6571b428fc572f5227e9
Bosco Ho <apposite@gmail.com>
2024-04-06 11:28:25 +1100

    new example

--- dev.sh
+++ dev.sh
@@ -5,29 +5,24 @@
 psword -k node
 psword -k mambaforge
 psword -k rshow
-psword -k serve.py
 
 echo "$1"
 if [ "$1" == "traj" ]; then
     ttab "rshow --dev traj examples/trajectory.h5" ;
-elif [ "$1" == "foam" ] ; then
-    ttab "rshow --dev traj-foam 23" ;
 elif [ "$1" == "matrix" ]; then
     ttab "cd examples; rshow --dev matrix matrix" ;
 elif [ "$1" == "fes" ]; then
-    ttab "rshow --dev matrix examples/fes" ;
+    ttab "rshow --dev matrix examples/fes/fes.rshow.yaml" ;
 elif [ "$1" == "scan1" ]; then
-    ttab "cd examples/scan1; rshow --dev fes " ;
-elif [ "$1" == "temper" ]; then
-    ttab "cd examples/temper; rshow --dev re ." ;
+    ttab "cd examples/scan1; rshow --dev matrix scan1/fes.rshow.yaml " ;
 elif [ "$1" == "frame" ]; then
     ttab "rshow --dev frame examples/3hhm.pdb" ;
 elif [ "$1" == "ligands" ]; then
-    ttab "cd examples/ligands; rshow --dev ligands 2vuk_220C.pdb 2vuk.oeb 2vuk.csv"
+    ttab "cd examples/ligands; rshow --dev ligands receptor.pdb fred100.sdf"
 elif [ "$1" == "cam3" ]; then
-    ttab "cd ~/work/cam3/model5; rshow --dev matrix matrix_fes0.yaml"
+    ttab "cd examples/model5; rshow --dev matrix matrix_fes0.yaml"
 elif [ "$1" == "cam3t" ]; then
-    ttab "cd ~/work/cam3/run5; rshow --dev fes"
+    ttab "cd examples/run5; rshow --dev matrix fes.rshow.yaml"
 else
     echo "Didn't recognize $1: traj foam matrix fes scan1 temper frame ligands"
     exit 1
