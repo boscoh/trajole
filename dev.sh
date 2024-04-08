94fe2de3e3dc010e0e6bd2bbaab1bd77570d5285
Bosco Ho <apposite@gmail.com>
2024-04-08 23:30:05 +1000

    fix

--- dev.sh
+++ dev.sh
@@ -1,6 +1,8 @@
 # install ttab using `npm i -g ttab`
 # on mac, need to change
 #   System Prefs -> Security -> Privacy -> Accessibility: add Terminal
+
+# check options for input example file
 if [[ "$1" =~ ^(traj|foam|matrix|fes|scan1|temper|frame|ligands|re|cam3t)$ ]]; then
     echo "opening $1 example"
 else
@@ -8,6 +10,7 @@ else
     exit 1
 fi
 
+# clear older running jobs that got orphaned
 if command -v psword &> /dev/null
 then
   psword -k MacOS/Python
@@ -16,8 +19,12 @@ then
   psword -k node
 fi
 
+# run server in dev mode with reload on port 9023 with chosen example
 ttab "cd examples; ./dev_server.sh $1"
+
+# run client server on port 3333
 ttab "cd client; npm run dev;"
 
+# open client on browser when server is ready
 source ./.venv/bin/activate
 rshow open-url http://localhost:9023 http://localhost:3333/#/foamtraj/0
